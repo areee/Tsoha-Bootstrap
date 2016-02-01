b61e77d7164269a43b31d7087faeb41fda3818de
Arttu Ylhävuori <areee@users.noreply.github.com>
2016-02-01 23:59:43 +0200

    sql database started

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,36 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+	id SERIAL PRIMARY KEY,
+	nimi varchar(50) NOT NULL,
+	salasana varchar(50) NOT NULL,
+	yllapito-oikeus boolean default false,
+	lisays-pvm DATE
+);
+
+CREATE TABLE Raaka-aine(
+	id SERIAL PRIMARY KEY,
+	nimi varchar(50) NOT NULL,
+	maara INTEGER NOT NULL,
+	yksikko NOT NULL,
+	lisays-pvm DATE,
+	muokkaus-pvm DATE
+);
+
+CREATE TABLE Resepti(
+	id SERIAL PRIMARY KEY,
+	nimi varchar(50) NOT NULL,
+	ohje varchar(1000) NOT NULL,
+	lahde varchar(50) NOT NULL,
+	lisays-pvm DATE,
+	muokkaus-pvm DATE
+);
+
+CREATE TABLE Kayttaja-Raaka-aine(
+kayttaja_id INTEGER REFERENCES Kayttaja(id)
+raaka-aine_id INTEGER REFERENCES Raaka-aine(id)
+
+);
+
+CREATE TABLE Raaka-aine-Resepti(
+
+);
