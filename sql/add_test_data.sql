5c54a23dfb3d0c761487e080303b590a49c90613
Arttu Ylhävuori <areee@users.noreply.github.com>
2016-02-08 10:19:21 +0200

    sql files updated

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,11 +1,21 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
-INSERT INTO Kayttaja (nimi,salasana,yllapitoOikeus,lisaysPvm) VALUES ('Arttu','salasana',true,'2015-12-12');
-INSERT INTO Kayttaja (nimi,salasana,yllapitoOikeus,lisaysPvm) VALUES ('Pekka','asdf123',false,'2016-02-01');
+INSERT INTO Chef (username,password,is_admin,added,updated) VALUES ('areee','salasana',true,'2015-12-12','2016-02-01');
+INSERT INTO Chef (username,password,is_admin,added,updated) VALUES ('asdf','asdf123',false,'2015-12-01','2016-02-01');
 
-INSERT INTO RaakaAine (nimi,maara,yksikko,lisaysPvm,muokkausPvm) VALUES ('Peruna',1,1,'2011-11-11',NOW());
+INSERT INTO Food (name,volume,unit,added,updated) VALUES ('Kiinteä harjattu peruna',3,'kg','2011-11-11',NOW());
+INSERT INTO Food (name,volume,unit,added,updated) VALUES ('Rasvaton maito',10,'l','2012-12-29','2016-01-01');
+INSERT INTO Food (name,volume,unit,added,updated) VALUES ('Kasvirasvalevite',0.4,'kg','2014-05-14','2016-01-31');
+INSERT INTO Food (name,volume,unit,added,updated) VALUES ('Meijerivoi',0.5,'kg','2015-06-19','2016-02-02');
+INSERT INTO Food (name,volume,unit,added,updated) VALUES ('Suomalainen hunaja',0.35,'kg','2015-06-19','2016-02-02');
 
-INSERT INTO Resepti (nimi,ohje,lahde,lisaysPvm,muokkausPvm) VALUES ('Paistetut perunat','Paista perunat rasvassa pannulla.','omasta päästä','2010-10-10','2015-12-31');
+INSERT INTO Recipe (name,food_id,volume,unit,instructions,source,portions,added,updated) VALUES ('Paistetut perunat','{1,3}','{1,200}','{kg,g}','Kuumenna margariini paistinpannulla.\nLisää pilkotut perunat.\n Paista 10 min miedolla lämmöllä.','omasta päästä',1,'2010-10-10',NOW());
+INSERT INTO Recipe (name,food_id,volume,unit,instructions,source,portions,added,updated) VALUES ('Hunajamaito','{2,5}','{0.2,1}','{l,rkl}','Lämmitä maito kuumaksi mikroaaltouunissa.\nLisää hunaja.\nAnna jäähtyä hetki. \nNauti!','vanhemmat',1,'2007-08-10',NOW());
 
-INSERT INTO KayttajaRaakaAine (kayttaja_id,raakaaine_id) VALUES (1,1);
+INSERT INTO ChefFood (chef.id,food.id) VALUES (1,1);
+INSERT INTO ChefFood (chef.id,food.id) VALUES (1,2);
+INSERT INTO ChefFood (chef.id,food.id) VALUES (1,3);
+INSERT INTO ChefFood (chef.id,food.id) VALUES (1,5);
+INSERT INTO ChefFood (chef.id,food.id) VALUES (2,4);
 
-INSERT INTO RaakaAineResepti (raakaaine_id,resepti_id) VALUES (1,1);
+INSERT INTO ChefRecipe (chef.id,recipe.id) VALUES (1,1);
+INSERT INTO ChefRecipe (chef.id,recipe.id) VALUES (1,2);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -20,9 +20,9 @@ CREATE TABLE Food(
 CREATE TABLE Recipe(
 	id SERIAL PRIMARY KEY,
 	name varchar(50) NOT NULL,
-	food_id INTEGER REFERENCES Food(id),
-	volume INTEGER,
-	unit varchar(20),
+	food_id integer[] REFERENCES Food(id) NOT NULL,
+	volume integer[],
+	unit text[],
 	instructions varchar(1000) NOT NULL,
 	source varchar(50) NOT NULL,
 	portions INTEGER NOT NULL,
