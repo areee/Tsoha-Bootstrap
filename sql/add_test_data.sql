b402469c884bee21868bcaf70df42bc415ecc500
Arttu Ylhävuori <areee@users.noreply.github.com>
2016-02-08 11:32:29 +0200

    sql files updated

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,6 +1,6 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
-INSERT INTO Chef (username,password,is_admin,added,updated) VALUES ('areee','salasana',true,'2015-12-12','2016-02-01');
-INSERT INTO Chef (username,password,is_admin,added,updated) VALUES ('asdf','asdf123',false,'2015-12-01','2016-02-01');
+INSERT INTO Chef (username,password,is_admin,added,updated) VALUES ('areee','salasana',TRUE,'2015-12-12','2016-02-01');
+INSERT INTO Chef (username,password,is_admin,added,updated) VALUES ('asdf','asdf123',FALSE,'2015-12-01','2016-02-01');
 
 INSERT INTO Food (name,volume,unit,added,updated) VALUES ('Kiinteä harjattu peruna',3,'kg','2011-11-11',NOW());
 INSERT INTO Food (name,volume,unit,added,updated) VALUES ('Rasvaton maito',10,'l','2012-12-29','2016-01-01');
@@ -8,14 +8,19 @@ INSERT INTO Food (name,volume,unit,added,updated) VALUES ('Kasvirasvalevite',0.4
 INSERT INTO Food (name,volume,unit,added,updated) VALUES ('Meijerivoi',0.5,'kg','2015-06-19','2016-02-02');
 INSERT INTO Food (name,volume,unit,added,updated) VALUES ('Suomalainen hunaja',0.35,'kg','2015-06-19','2016-02-02');
 
-INSERT INTO Recipe (name,food_id,volume,unit,instructions,source,portions,added,updated) VALUES ('Paistetut perunat','{1,3}','{1,200}','{kg,g}','Kuumenna margariini paistinpannulla.\nLisää pilkotut perunat.\n Paista 10 min miedolla lämmöllä.','omasta päästä',1,'2010-10-10',NOW());
-INSERT INTO Recipe (name,food_id,volume,unit,instructions,source,portions,added,updated) VALUES ('Hunajamaito','{2,5}','{0.2,1}','{l,rkl}','Lämmitä maito kuumaksi mikroaaltouunissa.\nLisää hunaja.\nAnna jäähtyä hetki. \nNauti!','vanhemmat',1,'2007-08-10',NOW());
+INSERT INTO Recipe (name,volume,unit,instructions,source,portions,added,updated) VALUES ('Paistetut perunat','{1,200}','{"kg","g"}',E'Kuumenna margariini paistinpannulla.\nLisää pilkotut perunat.\nPaista 10 min miedolla lämmöllä.','omasta päästä',1,'2010-10-10',NOW());
+INSERT INTO Recipe (name,volume,unit,instructions,source,portions,added,updated) VALUES ('Hunajamaito','{0.2,1}','{"l","rkl"}',E'Lämmitä maito kuumaksi mikroaaltouunissa.\nLisää hunaja.\nAnna jäähtyä hetki.\nNauti!','vanhemmat',1,'2007-08-10',NOW());
 
-INSERT INTO ChefFood (chef.id,food.id) VALUES (1,1);
-INSERT INTO ChefFood (chef.id,food.id) VALUES (1,2);
-INSERT INTO ChefFood (chef.id,food.id) VALUES (1,3);
-INSERT INTO ChefFood (chef.id,food.id) VALUES (1,5);
-INSERT INTO ChefFood (chef.id,food.id) VALUES (2,4);
+INSERT INTO ChefFood (chef_id,food_id) VALUES (1,1);
+INSERT INTO ChefFood (chef_id,food_id) VALUES (1,2);
+INSERT INTO ChefFood (chef_id,food_id) VALUES (1,3);
+INSERT INTO ChefFood (chef_id,food_id) VALUES (1,5);
+INSERT INTO ChefFood (chef_id,food_id) VALUES (2,4);
 
-INSERT INTO ChefRecipe (chef.id,recipe.id) VALUES (1,1);
-INSERT INTO ChefRecipe (chef.id,recipe.id) VALUES (1,2);
+INSERT INTO ChefRecipe (chef_id,recipe_id) VALUES (1,1);
+INSERT INTO ChefRecipe (chef_id,recipe_id) VALUES (1,2);
+
+INSERT INTO RecipeFood (recipe_id,food_id) VALUES (1,1);
+INSERT INTO RecipeFood (recipe_id,food_id) VALUES (1,3);
+INSERT INTO RecipeFood (recipe_id,food_id) VALUES (2,2);
+INSERT INTO RecipeFood (recipe_id,food_id) VALUES (2,5);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,15 +3,15 @@ CREATE TABLE Chef(
 	id SERIAL PRIMARY KEY,
 	username varchar(20) NOT NULL,
 	password varchar(20) NOT NULL,
-	is_admin boolean default false NOT NULL,
-	added DATE
+	is_admin boolean default false ,
+	added DATE,
 	updated DATE
 );
 
 CREATE TABLE Food(
 	id SERIAL PRIMARY KEY,
 	name varchar(50) NOT NULL,
-	volume INTEGER NOT NULL,
+	volume double precision NOT NULL,
 	unit varchar(20) NOT NULL,
 	added DATE,
 	updated DATE
@@ -20,8 +20,7 @@ CREATE TABLE Food(
 CREATE TABLE Recipe(
 	id SERIAL PRIMARY KEY,
 	name varchar(50) NOT NULL,
-	food_id integer[] REFERENCES Food(id) NOT NULL,
-	volume integer[],
+	volume double precision[],
 	unit text[],
 	instructions varchar(1000) NOT NULL,
 	source varchar(50) NOT NULL,
@@ -31,13 +30,16 @@ CREATE TABLE Recipe(
 );
 
 CREATE TABLE ChefFood(
-chef.id INTEGER REFERENCES Chef(id),
-food.id INTEGER REFERENCES Food(id)
-
+chef_id INTEGER REFERENCES Chef(id),
+food_id INTEGER REFERENCES Food(id)
 );
 
 CREATE TABLE ChefRecipe(
-chef.id INTEGER REFERENCES Chef(id),
-recipe.id INTEGER REFERENCES Recipe(id)
+chef_id INTEGER REFERENCES Chef(id),
+recipe_id INTEGER REFERENCES Recipe(id)
+);
 
+CREATE TABLE RecipeFood(
+recipe_id INTEGER REFERENCES Recipe(id),
+food_id INTEGER REFERENCES Food(id)
 );
